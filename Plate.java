3bee99f429509060b0f0b626b5850374fcc0020c
tlsimala <50175853+tlsimala@users.noreply.github.com>
2020-03-19 18:09:22 -0400

    Update Plate.java

--- Plate.java
+++ Plate.java
@@ -3,17 +3,36 @@ import java.util.HashSet;
 public class Plate {
 	private String ID;
 	private HashSet<Well> wellSet;
-	
+	/**
+	 * Constructor
+	 * @param plateID
+	 */
 	public Plate(String plateID) {
 		this.ID=plateID;
 		this.wellSet=new HashSet<Well>();
 	}
+	
+	/**
+	 * Gets the plate ID
+	 * @return
+	 */
 	public String getID() {
 		return ID;
 	}
+	
+	/**
+	 * Adds well to the plate
+	 * @param well
+	 */
 	public void addCompoundToAWell(Well well) {
 		wellSet.add(well);
 	}
+	
+	/**
+	 * Returns the well compound
+	 * @param wellID
+	 * @return
+	 */
 	public String returnCompound(String wellID) {
 		for(Well well: wellSet) {
 			if(well.getID().equals(wellID)) {
