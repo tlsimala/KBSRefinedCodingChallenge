76a0e5c5fcea920aa7252ad58d727cd69ad2fa12
tlsimala <50175853+tlsimala@users.noreply.github.com>
2020-03-19 16:40:35 -0400

    Update Well.java
    added comments

--- Well.java
+++ Well.java
@@ -1,13 +1,26 @@
 public class Well {
 	private String ID;
 	private String compound;
+	/**
+	 * The constructor
+	 * @param wellId
+	 * @param compound String
+	 */
 	public Well(String wellID, String compoundString) {
 		this.ID=wellID;
 		this.compound=compoundString;
 	}
+	/**
+	 * This method returns the ID
+	 * @param args
+	 */
 	public String getID() {
 		return ID;
 	}
+	/**
+	 * This method gets the type of compound associated with the well
+	 * @param args
+	 */
 	public String getCompound() {
 		return compound;
 	}
